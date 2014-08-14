2afaaa8fae84be3fe3164ef73fa7a9f4e653d211
xiangxiang <cobola@gmail.com>
2014-08-15 05:26:27 +0800

    update

--- chrome-plugin-qrcode/popup.js
+++ chrome-plugin-qrcode/popup.js
@@ -3,14 +3,30 @@
 // found in the LICENSE file.
 
 
-chrome.tabs.query({active:true},function(tab){
-    url = tab[0].url;
-    $('#qrcode').qrcode(url);
+chrome.tabs.query({
+	active: true
+}, function(tab) {
+	url = tab[0].url;
+	$('#qrcode').qrcode(url);
 });
 
 function click(e) {
-	
+
+alert(e.target.id);
+	if (e.target.id == 'download') {
+		download();
+	}
 	window.close();
+}
+
+function download() {
+	
+	// here is the most important part because if you dont replace you will get a DOM 18 exception.  
+	var image = $("#qrcode").toDataURL("image/png").replace("image/png", "image/octet-stream;Content-Disposition: attachment;filename=foobar.png");  
+	// var image = myCanvas.toDataURL("image/png").replace("image/png", "image/octet-stream");
+	// window.location.href = image; // it will save locally  
+	console.log(image);
+	window.open(image);
 }
 
 document.addEventListener('DOMContentLoaded', function() {
